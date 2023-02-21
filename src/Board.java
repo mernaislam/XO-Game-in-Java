dbb64b9ff871846b902bbbae4d8484f5b60d48fb
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2023-02-21 23:18:11 +0200

    Edits

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,7 @@
 public abstract class Board {
     protected int n;
     protected int n_moves = 0;
-    protected char board[][];  // char board[][] = new char[3][3];
+    protected char board[][];
     public Board(int num){
         n = num;
         board = new char[n][n];
@@ -29,18 +29,3 @@ public abstract class Board {
         }
     }
 }
-
-// abstract class : class that have at least one pure virtual function
-
-// funtion player virtual getMove() ==> welcome ...
-// computerPlayer getMove() ==> find best move ...
-// main ==>
-// Player p1;
-// computerPlayer pc;
-// p1.getMove();
-// pc.getMove();
-
-// Board virtual is_Winner() = 0;  ==> abstract is_winner();
-// XOboard is_Winner() { digaonal ... };
-// Connect4Board is_Winner() { };
-
--- src/Player.java
+++ src/Player.java
@@ -1,20 +1,16 @@
 import java.util.*;
-public class Player {   // p1 -> name , symbol , x , y ==> getX, getY
+public class Player {
     Scanner sc = new Scanner(System.in);
     private String name;
     private char symbol;
     private int x, y;
     public Player(String name, char symbol){
-        // cout << .... ; ==== System.out.print("..." + variable);
-        // cout << "kalam" << variable << .. << endl;
-        System.out.println("Welcome player: " + name);  // cout << "welcome player: " << name << endl;
+        System.out.println("Welcome player: " + name);
         this.symbol = symbol;
         this.name = name;
     }
     public void getMove(){
         System.out.println("Enter your position in rows and columns (1:3) separated by spaces: " );
-        // cin >> x;
-        // char ayhaga = sc.next().charAt(0);
         x = sc.nextInt();
         y = sc.nextInt();
         x--;
